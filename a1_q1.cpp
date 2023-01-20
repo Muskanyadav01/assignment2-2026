aa734af41b22ee8b847bdd5ed72f2c2297bfdb40
Muskanyadav01 <121680274+Muskanyadav01@users.noreply.github.com>
2023-01-20 20:53:18 +0530

    submitting answer 1 of assignment 2

---
+++ a1_q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the number of rows you want to be printed."<<endl;
+    cin >> n;
+    
+   //write your code here
+   for(int row= 1; row<=n; row++){
+
+       for(int col=1; col<=row; col++){
+         cout<< " * ";
+       } 
+       cout<<endl;
+   }
+}
